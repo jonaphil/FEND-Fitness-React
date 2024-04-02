d4133eca00e8a69c67326050f455ec67d1dfb19d
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-04-02 16:46:19 +0200

    Tailwind set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,30 @@
 module.exports = {
   content: ["./src/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      ddark: "#202430",
+      dmedium: "#3A4151",
+      dlight: "#FBFBFB",
+      gradient1: "linear-gradient(315deg, #FF99C4 0%, #FFD162 100%)",
+      gradient2: "linear-gradient(315deg, #F5FFA0 0%, #3EF3E8 100%)",
+      gradient3:
+        "linear-gradient(315deg, #3A4AE4 0%, #3B85E6 21.35%, #3EE4E8 73.44%, #3EF3E8 92.71%)",
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    fontSize: {
+      xxl: "2.25rem",
+      xl: "1.5rem",
+      l: "1.125rem",
+      m: "1rem",
+      s: "0.75rem",
+      xs: "0.5rem",
+    },
+    fontWeight: {
+      bold: 700,
+      regular: 400,
+    },
     extend: {},
   },
   plugins: [],
