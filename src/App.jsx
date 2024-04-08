d1371e6fed9b7b184179be9c33a01cf8eb47954b
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-04-08 20:43:34 +0200

    change size of the main-div in the app

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Dashboard from "./compontents/Dashboard.jsx";
 
 function App() {
   return (
-    <>
+    <div className="max-w-[414px] max-h-[736px]">
       <Dashboard name={"Jonathan"} currentExcercise={"Exc3"} />
-    </>
+    </div>
   );
 }
 
