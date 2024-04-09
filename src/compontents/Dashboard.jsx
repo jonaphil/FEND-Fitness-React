fd13b4111b05d96fcd83b63ddff604b412ce64de
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-04-09 10:33:13 +0200

    Basic Dashboard and Navi prototype working

--- src/compontents/Dashboard.jsx
+++ src/compontents/Dashboard.jsx
@@ -7,26 +7,25 @@ export default function Dashboard({ name, currentExcercise }) {
   console.log(currentExcercise);
   return (
     <div className="bg-ddark flex flex-col gap-6">
-      <div className="pl-4 pr-5 pt-10 flex flex-col items-start">
-        <h1 className="">Hi, {name}!</h1>
+      <div className="pl-4 pr-5 pt-10 flex flex-col items-start gap-3">
+        <h1 className="">Hi {name}!</h1>
         <img
           className="self-center ml-16 mr-14 mt-6 mb-10"
           src="../media/images/Layer 11 1.png"
           alt="Picture of a human, stretching in the morning"
         />
         <div className="self-stretch flex justify-between items-center">
-          <h2>Dein Trainingsplan</h2>
-          <a href="Trainingsplan" className="fontSize-s">
-            Dein Trainingsplan
+          <h2>Dein Workout heute</h2>
+          <a href="Trainingsplan" className="text-xs">
+            Trainingsplan
           </a>
         </div>
 
-        <div className="rounded-[30px] bg-dmedium self-stretch p-8">
-          <div className="h-1/2"></div>
-          <div>
+        <div className="rounded-[30px] bg-dmedium self-stretch p-8 w-93.75 h-53.75 flex flex-col justify-end">
+          <div className="">
             <h3>Tag 2</h3>
             <h2>Titel des Programms</h2>
-            <p className="fontSize-s">26 Min. - Beweglichkeit</p>
+            <p className="text-xs">26 Min. - Beweglichkeit</p>
           </div>{" "}
         </div>
       </div>
--- src/compontents/Navi.jsx
+++ src/compontents/Navi.jsx
@@ -9,17 +9,17 @@ export default function Navi({ activeButton }) {
   //activeButton = one of {'home', 'dumbbell', 'profile'}
 
   return (
-    <div className="relative bottom-0 pt-3 pb-3 w-full bg-black bg-opacity-40 rounded-t-[20px] flex justify-around">
-      <a className="w-[22px]" href="/home">
+    <div className="relative bottom-0 pt-3 pb-3 pl-12 pr-13 w-full bg-black bg-opacity-40 rounded-t-[20px] flex justify-between items-center box-border">
+      <a className="w-[25px]" href="/home">
         <img src={activeButton == "home" ? homeWhite : homeGrey} alt="" />
       </a>
-      <a className="w-[34px]" href="/workout-list">
+      <a className="w-[37px]" href="/workout-list">
         <img
           src={activeButton == "dumbbell" ? dumbbellWhite : dumbbellGrey}
           alt=""
         />
       </a>
-      <a className="w-[25px]" href="/profile">
+      <a className="w-[28px]" href="/profile">
         <img
           src={activeButton == "profile" ? profileWhite : profileGrey}
           alt=""
