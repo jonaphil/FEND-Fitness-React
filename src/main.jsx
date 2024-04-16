5c11bbc2820ebb459b00158e6757d123fd0a9027
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-04-16 15:53:25 +0200

    Created Router And Apollo Provieder in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,36 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
+// React general
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+// React Router TODO
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+//Apollo TODO
+const clientApollo = new ApolloClient({
+  uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cluu29pkz000008l91dji8p5l/master",
+  cache: new InMemoryCache(),
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={clientApollo}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
   </React.StrictMode>
 );
