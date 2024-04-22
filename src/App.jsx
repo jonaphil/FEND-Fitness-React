01c38f71bd3c05c1f15b837de0ea03cb43291b7d
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-04-22 23:09:54 +0200

    modified App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./output.css";
 import Dashboard from "./pages/Dashboard.jsx";
 import ExcerciseList from "./pages/ExcerciseList.jsx";
+import Profile from "./pages/Profile.jsx";
 
 function App() {
   return (
-    <div className="w-[414px] h-[736px]">
-      <Dashboard name={"Name"} currentExcercise={"Exc3"} />
+    <>
+      {/*<Dashboard name={"Name"} currentExcercise={"Exc3"} />*/}
+      <Profile />
       {/* <ExcerciseList /> */}
-    </div>
+      {/* <Profile /> */}
+    </>
   );
 }
 
