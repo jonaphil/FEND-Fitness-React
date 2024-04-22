57df7cf6ae51f89d992d807c5c014e4be58712c6
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-04-22 23:06:13 +0200

    Modified Component MainScreen and Navi
    Added Link from React Router

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,11 +1,11 @@
 import Navi from "./Navi";
 
 export default function MainScreen({ page = "home", children }) {
-  const pageButton = page === "browseList" ? "dumbbell" : page;
+  const pageButton = page === "excerciseList" ? "dumbbell" : page;
 
   return (
-    <div className="h-full w-full bg-ddark relative">
-      <div className="h-full pt-10 pl-4 pr-4.5 pb-18 flex flex-col gap-3 items-center justify-center box-border">
+    <div className="min-h-screen w-full bg-ddark relative">
+      <div className="relative h-full pt-10 pl-4 pr-4.5 pb-17.5 flex flex-col gap-3 items-center justify-center box-border">
         {children}
       </div>
 
