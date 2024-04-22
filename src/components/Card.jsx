5d772cc58d09e9030b2b5bd7beaae37d87d3ced7
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-04-22 23:05:01 +0200

    modified Component: Card
    Added size-argument to have a more flexible Component.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,25 @@
-export default function Card({ bgColor, justify, className = "", children }) {
-  // TODO Define different Card-Types!
+export default function Card({
+  bgColor,
+  size,
+  justify,
+  className = "",
+  children,
+}) {
+
+  switch (size) {
+    case "s":
+      return (
+      <div className={`w-full h-fit bg-${bgColor} pt-7 pb-8 pl-5 pr-5 rounded-2.5xl flex flex-row gap-6 items-start justify-${justify} ${className}`}>
+          {children}
+        </div>
+      )
+
+    default:
+      break;
+  }
   return (
     <div
-      className={`w-93.75 h-53.75 bg-${bgColor} p-12.5 rounded-2.5xl flex flex-col items-start justify-${justify} ${className}`}
+      className={`w-full h-53.75 bg-${bgColor} p-12.5 rounded-2.5xl flex flex-col items-start justify-${justify} ${className}`}
     >
       {children}
     </div>
