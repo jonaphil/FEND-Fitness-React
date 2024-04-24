96b24e52d3255e5e7abf49b938cb5fe768cc0dba
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-04-24 13:07:44 +0200

    tailwind.config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   content: ["./src/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   safelist: [
+    // This is needed for classes which are dynamically named and applied so tailwind doesn't know beforehand they are used.
+    // This is for the Card Component
     "bg-gradient-red",
     "bg-gradient-green",
     "bg-gradient-blue",
@@ -16,7 +18,6 @@ module.exports = {
     "shadow-m",
     "shadow-m-strong",
     "shadow-l",
-    "border-dlight",
   ],
   theme: {
     extend: {
