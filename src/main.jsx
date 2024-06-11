984f948d03341b5834a54521dc9ec77c73b021bb
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-06-11 23:43:21 +0200

    minor tinkering Updates

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ import Workout from "./components/Workout";
 import Exercise from "./components/Exercise";
 import loadProgramsList from "./queries/programsList";
 import loadProgramDetails from "./queries/getProgramDetails";
+import loadWorkoutDetails from "./queries/getWorkout";
 
 // React general
 const container = document.getElementById("root");
@@ -42,7 +43,9 @@ const router = createBrowserRouter([
   {
     path: "/program/:programId/",
     element: <ProgramComp />,
-    loader: loadProgramDetails,
+    loader: ({ params }) => {
+      return loadProgramDetails(params);
+    },
     children: [
       {
         path: "details/",
@@ -55,9 +58,13 @@ const router = createBrowserRouter([
       {
         path: "workout/:workoutId/",
         element: <Workout />,
+        id: "workout",
+        loader: ({ params }) => {
+          return loadWorkoutDetails(params);
+        },
         children: [
           {
-            path: ":ExerciseIndex/",
+            path: ":exerciseIndex/",
             element: <Exercise />,
           },
         ],
