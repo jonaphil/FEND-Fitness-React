99bec6bafd99954b68a8a07a9be3b331f5357c8e
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-06-11 23:41:44 +0200

    updated Program-Page and -list

--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -1,36 +1,36 @@
-import { Outlet, useParams } from "react-router-dom";
-import getProgramDetails from "../queries/getProgramDetails";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+import { Outlet } from "react-router-dom";
 import { getProgramStats } from "./ProgramDetails";
 import { LoadingPage } from "../components/StatusElements/Loading";
-import Error from "../components/StatusElements/Error";
+import { ErrorPage } from "../components/StatusElements/Error";
 import { ProgramContext } from "../Context";
-
-export default function Program() {
-  const { programId } = useParams();
-
-  const programQuery = getProgramDetails(programId);
-
-  if (programQuery.loading) {
-    return <LoadingPage />;
-  }
-  if (programQuery.error) {
-    console.log(
-      `An Error occured fetching the program data: ${programQuery.error.message}`
-    );
-    return <Error />;
-  }
-
-  const programObj = {
-    ...programQuery?.data?.programs[0],
-    stats: getProgramStats(programQuery?.data?.programs[0].workoutsWithDay),
-  };
-  return (
-    <main className="bg-ddark">
+import { useLoaderData } from "../../node_modules/react-router-dom/dist/index";
+export function ProgramComp() {
+    var _a, _b;
+    var programQuery = useLoaderData();
+    if (programQuery.loading) {
+        return <LoadingPage />;
+    }
+    if (programQuery.error) {
+        console.log("An Error occured fetching the program data: ".concat(programQuery.error.message));
+        return <ErrorPage />;
+    }
+    var programObj = __assign(__assign({}, (_a = programQuery === null || programQuery === void 0 ? void 0 : programQuery.data) === null || _a === void 0 ? void 0 : _a.programs[0]), { stats: getProgramStats((_b = programQuery === null || programQuery === void 0 ? void 0 : programQuery.data) === null || _b === void 0 ? void 0 : _b.programs[0].workoutsWithDay) });
+    return (<main className="bg-ddark">
       <ProgramContext.Provider value={programObj}>
-        <div className="h-screen w-full bg-ddark">
+        <div className="min-w-screen flex min-h-screen flex-col items-stretch justify-stretch bg-ddark">
           <Outlet />
         </div>
       </ProgramContext.Provider>
-    </main>
-  );
+    </main>);
 }
