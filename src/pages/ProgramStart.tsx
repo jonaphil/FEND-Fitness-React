93cc44d3d1f276954487fcabc341c9eb8787e864
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-06-11 23:42:11 +0200

    Updated ProgramStart-Page

--- src/pages/ProgramStart.tsx
+++ src/pages/ProgramStart.tsx
@@ -1,34 +1,52 @@
 import React from "react";
 import { useContext } from "react";
+import { Link } from "../../node_modules/react-router-dom/dist/index";
 import Button from "../components/Button";
-import { ProgramContext } from "../Context";
+import { ProgramContext, ProgramType } from "../Context";
+import ButtonGoBack from "../media/icons/ButtonGoBack.svg?react";
+import germanTranslation from "../helpers/translations";
 
-export interface Program {
-  name: String;
-  difficulty: "easy" | "moderate" | "hard";
-  focus: "mobility" | "cardio" | "weightTraining" | "coordination";
-  duration: Number;
-  description: String;
-  workoutsWithDay: Array<{
-    id: String;
-    day: Number;
-    workout: {
-      name: String;
-      category: "mobility" | "cardio" | "weightTraining" | "coordination";
-      duration: Number;
-    };
-  }>;
+interface HeaderProps {
+  absolute: Boolean;
+  text: String;
 }
 
 export function ProgramStart(): React.JSX.Element {
-  const program: Program = useContext(ProgramContext);
+  const program = useContext(ProgramContext) as ProgramType;
+  const currentDay = 1; // FIXME
+  const currentWorkout =
+    program.workoutsWithDay[(currentDay - 1) % program.workoutsWithDay.length]
+      .workout;
   return (
-    <div className="justify-betweeen flex flex-col">
-      <div className="flex flex-row justify-between">
-        <div></div>
-        <p className="text-2xs">{program.name}</p>
-        <svg></svg>
+    <div className="relative flex min-h-screen w-full flex-col items-center justify-between p-4 pb-16">
+      <Header absolute={false} text={program.name} />
+      <div className="flex flex-col items-center gap-4">
+        <h1>Tag {currentDay}</h1>
+        <p className="text-xs">
+          {`${currentWorkout.duration} Min. ${String.fromCharCode(0x00b7)} ${
+            germanTranslation[currentWorkout.category]
+          }`}
+        </p>
       </div>
+      <Link to={`../workout/${currentWorkout.id}/0`}>
+        <Button>los</Button>
+      </Link>
+    </div>
+  );
+}
+
+function Header({ absolute, text }: HeaderProps): React.JSX.Element {
+  return (
+    <div
+      className={`${
+        absolute && "absolute"
+      } flex w-full flex-row justify-between`}
+    >
+      <div className="w-5"></div>
+      <p className="justify-self-center text-2xs">{text}</p>
+      <Link to={`../details`}>
+        <ButtonGoBack />
+      </Link>
     </div>
   );
 }
