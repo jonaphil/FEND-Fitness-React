219b4b3ffd6dc16db9b8c8b44b54e7bec567687c
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-07-05 19:36:11 +0200

    added double check confirmation

---
+++ src/contexts/hooks/index.js
@@ -0,0 +1,1 @@
+export { default as useUserContext } from "./useUserContext.jsx";
---
+++ src/contexts/hooks/useUserContext.jsx
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { UserContext } from "@contexts/Context";
+import PROGRAM_QUERY from "@adapters/graphQL/queries/getProgramDetails";
+
+export default function useUserContext() {
+  const [user, setUser] = useContext(UserContext);
+  const navigate = useNavigate();
+  console.log(user);
+
+  const setUserProgram = (program) => {
+    const { id, name, duration, workoutsWithDay } = program;
+    console.log(program);
+    const { workout } = workoutsWithDay[0];
+    setUser({
+      ...user,
+      current: {
+        day: 1,
+        programId: id,
+        programName: name,
+        length: duration * 7,
+        progress: 0,
+        workout: {
+          id: workout.id,
+          duration: workout.duration,
+          focus: workout.category,
+        },
+      },
+    });
+    //TODO Post changed Data for user to server!
+    navigate("/training/start/");
+  };
+
+  return { user, setUserProgram };
+}
