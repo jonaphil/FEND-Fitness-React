9cdc747ce742dad4c0e2618759b580da47eb21b1
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-07-22 11:08:38 +0200

    Refactor Program Page with Defer-Await

--- src/views/Program/index.jsx
+++ src/views/Program/index.jsx
@@ -10,26 +10,32 @@ var __assign = (this && this.__assign) || function () {
     return __assign.apply(this, arguments);
 };
 import { Suspense } from "react";
-import { Outlet, Await } from "react-router-dom";
+import { Outlet, Await, useLoaderData } from "react-router-dom";
+import { useReadQuery } from "@apollo/client";
 import getProgramStats from "@utils/helpers/getProgramStats";
 import LoadingPage from "@views/StatusPages/Loading";
 import ErrorPage from "@views/StatusPages/Error";
 import { ProgramContext } from "@contexts/Context";
-import { useLoaderData } from "react-router-dom";
 export default function Program() {
-    var data = useLoaderData();
+    var queryRefPromise = useLoaderData().queryRefPromise;
     return (<main className="bg-ddark">
       <Suspense fallback={<LoadingPage />}>
-        <Await resolve={data.promise} errorElement={ErrorPage}>
-          {function (promise) {
-            var _a, _b;
-            return (<ProgramContext.Provider value={__assign(__assign({}, (_a = promise === null || promise === void 0 ? void 0 : promise.data) === null || _a === void 0 ? void 0 : _a.programs[0]), { stats: getProgramStats((_b = promise === null || promise === void 0 ? void 0 : promise.data) === null || _b === void 0 ? void 0 : _b.programs[0].workoutsWithDay) })}>
-              <div className="min-w-screen flex h-screen flex-col items-stretch overflow-scroll bg-ddark">
-                <Outlet />
-              </div>
-            </ProgramContext.Provider>);
-        }}
+        <Await resolve={queryRefPromise} errorElement={ErrorPage}>
+          {function (queryRef) { return (<ProgramContextResolved queryRef={queryRef}>
+              <Outlet />
+            </ProgramContextResolved>); }}
         </Await>
       </Suspense>
     </main>);
+}
+function ProgramContextResolved(_a) {
+    var queryRef = _a.queryRef, children = _a.children;
+    var data = useReadQuery(queryRef).data;
+    return (<>
+      <ProgramContext.Provider value={__assign(__assign({}, data.programs[0]), { stats: getProgramStats(data.programs[0].workoutsWithDay) })}>
+        <div className="min-w-screen flex h-screen flex-col items-stretch overflow-scroll bg-ddark">
+          {children}
+        </div>
+      </ProgramContext.Provider>
+    </>);
 }
--- src/views/Program/index.tsx
+++ src/views/Program/index.tsx
@@ -1,34 +1,43 @@
 import { Suspense } from "react";
-import { Outlet, Await } from "react-router-dom";
+import { Outlet, Await, useLoaderData } from "react-router-dom";
+import { useReadQuery } from "@apollo/client";
 import getProgramStats from "@utils/helpers/getProgramStats";
 import LoadingPage from "@views/StatusPages/Loading";
 import ErrorPage from "@views/StatusPages/Error";
 import { ProgramContext } from "@contexts/Context";
-import { useLoaderData } from "react-router-dom";
 
 export default function Program(): React.JSX.Element {
-  const data = useLoaderData();
+  const { queryRefPromise } = useLoaderData();
 
   return (
     <main className="bg-ddark">
       <Suspense fallback={<LoadingPage />}>
-        <Await resolve={data.promise} errorElement={ErrorPage}>
-          {(promise) => (
-            <ProgramContext.Provider
-              value={{
-                ...promise?.data?.programs[0],
-                stats: getProgramStats(
-                  promise?.data?.programs[0].workoutsWithDay
-                ),
-              }}
-            >
-              <div className="min-w-screen flex h-screen flex-col items-stretch overflow-scroll bg-ddark">
-                <Outlet />
-              </div>
-            </ProgramContext.Provider>
+        <Await resolve={queryRefPromise} errorElement={ErrorPage}>
+          {(queryRef) => (
+            <ProgramContextResolved queryRef={queryRef}>
+              <Outlet />
+            </ProgramContextResolved>
           )}
         </Await>
       </Suspense>
     </main>
+  );
+}
+
+function ProgramContextResolved({ queryRef, children }) {
+  const { data } = useReadQuery(queryRef);
+  return (
+    <>
+      <ProgramContext.Provider
+        value={{
+          ...data.programs[0],
+          stats: getProgramStats(data.programs[0].workoutsWithDay),
+        }}
+      >
+        <div className="min-w-screen flex h-screen flex-col items-stretch overflow-scroll bg-ddark">
+          {children}
+        </div>
+      </ProgramContext.Provider>
+    </>
   );
 }
