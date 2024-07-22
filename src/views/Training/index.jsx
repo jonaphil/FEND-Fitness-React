37105fe81ca24e0c4b9beaefba6cac01a0614299
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-07-22 11:13:43 +0200

    refactor Training Page

--- src/views/Training/index.jsx
+++ src/views/Training/index.jsx
@@ -1,6 +1,12 @@
+import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import Spinner from "@components/simple Components/Suspense/Spinner";
 export default function Program() {
-    return (<main className="min-w-screen flex h-screen flex-col items-stretch overflow-hidden bg-ddark">
-      <Outlet />
-    </main>);
+    return (<Suspense fallback={<div className="flex h-screen w-screen flex-col items-center justify-center">
+          <Spinner />
+        </div>}>
+      <main className="min-w-screen flex h-screen flex-col items-stretch overflow-hidden bg-ddark">
+        <Outlet />
+      </main>
+    </Suspense>);
 }
