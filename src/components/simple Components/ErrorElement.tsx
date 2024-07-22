17e48039191efcc550b14b1069421f0a679a44f2
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-07-22 08:39:23 +0200

    modified Error Element
    modified Error Element to use AsyncError

--- src/components/simple Components/ErrorElement.tsx
+++ src/components/simple Components/ErrorElement.tsx
@@ -1,4 +1,11 @@
+import { useAsyncError } from "react-router-dom";
+
 export default function ErrorElement() {
+  const asyncError = useAsyncError();
+
+  if (asyncError) {
+    console.log(asyncError);
+  }
   return (
     <div className="rounded-md bg-red-600 p-4">
       Sorry, an error occured. Please try again.
