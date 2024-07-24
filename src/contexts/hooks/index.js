613c65217a1e05e8721003fabd337d74ac8503cc
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-07-24 15:12:26 +0200

    update hooks/index

--- src/contexts/hooks/index.js
+++ src/contexts/hooks/index.js
@@ -1,5 +1,4 @@
 export { default as useUserContext } from "./useUserContext";
 export { default as useEntryCreator } from "./useEntryCreator";
 export { default as useAuthQuery } from "./useAuthQuery";
-export { default as usePopup } from "./usePopup";
 export { default as useFinish } from "./useFinish";
